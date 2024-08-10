97ddaf473df65d13ee42c2fc21ffccbc14ae9180
Jesus <albertchalare@gmail.com>
2024-08-09 22:58:13 -0500

    products app microsevice

---
+++ src/products/products.service.ts
@@ -0,0 +1,68 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    try {
+      this.$connect();
+      this.logger.log('Database connected');
+    } catch (error) {
+      throw new Error('Method not implemented.');
+    }
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({ data: createProductDto });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalPages = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        totalPages: totalPages,
+        page,
+        lastPage: lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
+
+    if (!product) {
+      throw new Error(`Product ${id} not found`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: __, ...data } = updateProductDto;
+    await this.findOne(id);
+    return this.product.update({ where: { id }, data });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    //this.product.delete({ where: { id } });
+    await this.product.update({ where: { id }, data: { available: false } });
+
+    return `Product #${id} removed`;
+  }
+}
