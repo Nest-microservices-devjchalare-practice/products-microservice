575731339448fd6135cdd66e244ef07627ea3bfd
Jesus <albertchalare@gmail.com>
2024-09-16 21:25:08 -0500

    validate products function

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -69,4 +69,21 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     return { message: `Product #${id} removed` };
   }
+
+  async validateProduct(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: { id: { in: ids }, available: true },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: `Some products were not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
 }
